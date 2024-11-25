ce883023df4a85e020eddc1501b4bf637779f06d
Liudmyla <savinska.l.y@gmail.com>
2024-11-25 11:43:19 +0100

    Introduced Create, Read, Update, and Delete (CRUD) operations for the Book entity via dedicated endpoints. Implemented repository pattern for MongoDB interactions and added required dependency injections. Updated project files to include necessary package references.

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Library.Database;
+using Library.Endpoints.Books;
+using Library.Endpoints.User;
+using Library.Options;
+using Library.Repositories;
+using Library.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", false)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom
+    .Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<MongoDbOptions>(builder.Configuration.GetSection("MongoDbOptions"));
+builder.Services.AddSingleton<IMongoDbConnectionFactory, MongoDbFactory>();
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<IAuthorizationTokenRepository, AuthorizationTokenRepository>();
+builder.Services.AddSingleton<IBookRepository, BookRepository>();
+builder.Services.AddSingleton<IUserAuthorizationService, UserAuthorizationService>();
+
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.MapCreateUser();
+app.MapLogIn();
+app.MapCreateBook();
+app.MapGetBooksByTitle();
+app.MapGetBooksByAuthor();
+app.MapDeleteBook();
+app.Run();
